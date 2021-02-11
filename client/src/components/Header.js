4370b1965086bb5f13a86ddd0b3015e17df99062
Sahil Prajapati <sahilprjpt206@gmail.com>
2021-02-11 07:17:55 +0530

    Add header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
 import { withStyles } from "@material-ui/core/styles";
-// import AppBar from "@material-ui/core/AppBar";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Map from "@material-ui/icons/Map";
-// import Typography from "@material-ui/core/Typography";
+
+import Context from '../context';
+
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import MapIcon from "@material-ui/icons/Map";
+import Typography from "@material-ui/core/Typography";
 
 const Header = ({ classes }) => {
-  return <div>Header</div>;
+  const { state } = useContext(Context);
+  const { currentUser } = state;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          {/* Title logo */}
+          <div className={classes.grow}>
+            <MapIcon className={classes.icon} />
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+            >
+              GeoPins
+            </Typography>
+          </div>
+
+          {currentUser && (
+            <div className={classes.grow}>
+              <img className={classes.picture}
+                src={currentUser.picture}
+                alt={currentUser.name}
+              />
+              <Typography
+                variant="h6"
+                color="inherit"
+                noWrap
+              >
+                {currentUser.name}
+              </Typography>
+            </div>
+          )}
+          {/* signup button */}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 };
 
 const styles = theme => ({
