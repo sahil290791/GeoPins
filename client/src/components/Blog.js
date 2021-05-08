90c22d884695a64ee878eb7f1316c49c8247ddd9
Sahil Prajapati <sahilprjpt206@gmail.com>
2021-05-08 22:59:07 +0530

    Show selected pin content

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -3,19 +3,22 @@ import { withStyles } from "@material-ui/core/styles";
 
 import NoContent from './Pin/NoContent';
 import CreatePin from './Pin/CreatePin';
+import PinContent from './Pin/PinContent';
 import { Paper } from "@material-ui/core";
 
 import Context from '../context';
 
 const Blog = ({ classes }) => {
   const { state } = useContext(Context);
-  const { draft } = state;
+  const { draft, currentPin } = state;
 
   let BlogContent;
-  if (!draft) {
+  if (!draft && !currentPin) {
     BlogContent = NoContent;
-  } else if (draft) {
+  } else if (draft && !currentPin) {
     BlogContent = CreatePin;
+  } else if (currentPin) {
+    BlogContent = PinContent;
   }
 
   return <Paper className={classes.root}>
--- client/src/components/Pin/PinContent.js
+++ client/src/components/Pin/PinContent.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
 import { withStyles } from "@material-ui/core/styles";
-// import Typography from "@material-ui/core/Typography";
-// import AccessTime from "@material-ui/icons/AccessTime";
-// import Face from "@material-ui/icons/Face";
+import format from "date-fns/format";
+
+import Context from '../../context';
+import Typography from "@material-ui/core/Typography";
+import AccessTime from "@material-ui/icons/AccessTime";
+import Face from "@material-ui/icons/Face";
 
 const PinContent = ({ classes }) => {
-  return <div>PinContent</div>;
+  const { state } = useContext(Context);
+  const { currentPin: { title, content, author, createdAt, comment } } = state;
+  return (
+    <div className={classes.root}>
+      <Typography
+        component="h2"
+        variant="h4"
+        color="primary"
+        gutterBottom
+      >
+        {title}
+      </Typography>
+      <Typography
+        className={classes.text}
+        variant="h6"
+        color="inherit"
+        gutterBottom
+        component="h3"
+      >
+        <Face className={classes.icon} /> {author.name}
+      </Typography>
+      <Typography className={classes.text} variant="subtitle2" color="inherit" gutterBottom>
+        <AccessTime className={classes.icon} /> {format(Number(createdAt), "MMM Do, YYYY")}
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom>
+        {content}
+      </Typography>
+    </div>
+  );
 };
 
 const styles = theme => ({
