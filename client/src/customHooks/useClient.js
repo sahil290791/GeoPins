1cd04bd03623b251d96d684df608505255aced2d
Sahil Prajapati <sahilprjpt206@gmail.com>
2021-05-10 09:11:53 +0530

    Update production graphql url

--- client/src/customHooks/useClient.js
+++ client/src/customHooks/useClient.js
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
 import { GraphQLClient } from 'graphql-request';
 
-export const BASE_URL = process.env.NODE_ENV === 'production' ? 'prod-url' : 'http://localhost:4000/graphql';
+export const BASE_URL = process.env.NODE_ENV === 'production' ?
+  'https://map-my-next-bike-ride.herokuapp.com/graphql' :
+  'http://localhost:4000/graphql';
 
 const useClient = () => {
   const [idToken, setIdToken] = useState('');
